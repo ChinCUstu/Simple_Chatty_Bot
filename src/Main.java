619ffc7186bb8c019923e61b0fff8be0768ef0a6
ChinCUstu <ndulue.chinedu@stu.cu.edu.ng>
2023-01-11 04:17:07 +0100

    Chatty Bot Guess the age

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
         System.out.println("Hello! My name is Ricky");
         System.out.println("I was created in 2023");
+        System.out.println("Please, remind me your name.");
+
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.next();
+        System.out.println("What a great name you have, " + name + "!");
     }
 }
 
